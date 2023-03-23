25dec4ac3289350e1b5de59a1971e20bb55e49b6
kineticstorytelling <kineticstorytelling@gmail.com>
2023-03-22 22:00:57 -0400

    'removed ignore'

--- src/config/firebase.config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyACg2H0aL5Q1W2K9v7TerZk0MSLGMprduo",
-  authDomain: "soul-soju.firebaseapp.com",
-  projectId: "soul-soju",
-  storageBucket: "soul-soju.appspot.com",
-  messagingSenderId: "1040999886059",
-  appId: "1:1040999886059:web:969cdc2d537ebe2175e2ae"
-};
-
